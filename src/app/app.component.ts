d5bae166cdd5b5b30f876decbdf08e53a266a236
anitaSF <ana.isabel.santos@bootcamp-upgrade.com>
2024-05-20 20:07:54 +0200

    greetings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { GreetingsComponent } from './components/greetings/greetings.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, GreetingsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 // En tipescript necesitamos indicar el tipo de variable que estamos creando. Para ello junto al nombre se especifica string, number, object
 export class AppComponent {
-  username: string = 'Anita';
 }
---
+++ src/app/components/greetings/greetings.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-greetings',
+  standalone: true,
+  imports: [],
+  templateUrl: './greetings.component.html',
+  styleUrl: './greetings.component.css'
+})
+export class GreetingsComponent {
+  username: string = 'Anita';
+
+}
